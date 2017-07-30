52bdb99dbd8d45a6638d0d594b5302a302e30b42
Tyg13 <animemachine13@gmail.com>
2017-07-30 15:45:37 -0700

    fixed boot.asm (s/||/|)

--- src/arch/x86_64/boot.asm
+++ src/arch/x86_64/boot.asm
@@ -136,8 +136,8 @@ enable_paging:
 section .rodata
 gdt64:
     dq 0 ; zero entry
-.code: equ $ - gdt44
-    dq (1 << 43) | (1 << 44) | (1 << 47) || (1 << 53) ; code segment
+.code: equ $ - gdt64
+    dq (1<<43) | (1<<44) | (1<<47) | (1<<53) ; code segment
 .pointer:
     dw $ - gdt64 - 1
     dq gdt64
